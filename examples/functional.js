3cbc99c1b273a0669eb8f45280d482dfb797e656
Dawid Szlachta <taheto@o2.pl>
2014-01-22 22:15:14 +0100

    fixed stringifiyVar, added examples/functional

---
+++ examples/functional.js
@@ -0,0 +1,13 @@
+
+var gen = require('../gen')();
+
+function higher1(citizenship) {
+    return function(name) {
+	return [name, 'is a', citizenship].join(' ');
+    }
+}
+
+var czech = higher1('Czech');
+var cache = gen.with_scope(gen.stringify(czech, 'Bohumil Hrabal'), {citizenship: 'Czech'});
+console.log(cache);
+console.log(eval(cache));
--- gen.js
+++ gen.js
@@ -1,8 +1,23 @@
 module.exports = function() {
 
+    function _get_args() {
+	var args;
+	
+	args = Array.prototype.slice.call(arguments, '');
+	fn = args.shift();
+
+	return {
+	    fn: fn,
+	    args: args
+	};
+    }
+
     var g = {
 	stringifyVar: function(v) {
-	    return eval(v);
+	    if (typeof v === 'string')
+		return "'" + v.toString() + "'";
+	    else
+		return eval(v);
 	},
 
 	stringifyFn: function(fn) {
@@ -10,20 +25,34 @@ module.exports = function() {
 	},
 
 	stringify: function(/*args*/) {
-	    var args;
+	    var data;
 	    var fn;
 	    var params;
 	    var res;
 	    
-	    args = Array.prototype.slice.call(arguments, '');
-	    fn = args.shift();
+	    data = _get_args.apply(null, arguments);
+	    fn = data.fn;
 	    
 	    res = '(' + g.stringifyFn(fn) + ')';
 
-	    params = args.map(g.stringifyVar);
+	    params = data.args.map(g.stringifyVar);
 	    res += '.call(null, ' + params.join(', ') + ');';
 	    
 	    return res;
+	},
+
+	with_scope: function(str, vars) {
+
+	    var res;
+	    var defs = [];
+
+	    for (var key in vars) {
+		if (vars.hasOwnProperty(key))
+		    defs.push('var ' + key + ' = ' + g.stringifyVar(vars[key]) + ";\n");
+	    }
+	
+	    res = (new Function(defs.join(' ') + 'return ' + str)).toString();
+	    return '(' + res + ')()';
 	}
 
     }
